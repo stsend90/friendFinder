8abe261731683af5e4beabea281b051502f82819
Jay\^.^/ <stsend90@yahoo.com>
2019-12-18 17:32:12 -0500

    deploy

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT);
+});
