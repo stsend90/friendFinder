bbbe4bd6803f4a5455b450e1108b0944ffc3a6ea
Jay\^.^/ <stsend90@yahoo.com>
2019-12-18 17:56:21 -0500

    fixed spelling

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 const friends = [{
     name:   "Batman",
-    photo:  "",
+    photo:  "https://imgix-media.wbdndc.net/cms/filer_public_thumbnails/filer_public/d8/5a/d85a3ec6-79c5-49ae-86e9-902c74546e69/batman-profile-293d6d-bm_cv17_ns-1-v1-600x600-marquee-thumb.jpg__600x600_q85_crop_subsampling-2_upscale.jpg",
     scores: [
         "5","1","4","3","2","5","1","4","3","1"
     ]
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,8 +1,8 @@
 const path = require("path");
 
 module.exports = function(app){
-    app.get("/survery", function(req, res){
-        res.sendFile(path.join(__dirname, "/..public/survey.html"));
+    app.get("/survey", function(req, res){
+        res.sendFile(path.join(__dirname, "/../public/survey.html"));
     });
 
 
