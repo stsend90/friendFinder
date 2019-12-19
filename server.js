69bcd504483b8cb7ed7c036ae1d2a39a74fe8e05
Jay\^.^/ <stsend90@yahoo.com>
2019-12-19 18:26:18 -0500

    update server.js

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
